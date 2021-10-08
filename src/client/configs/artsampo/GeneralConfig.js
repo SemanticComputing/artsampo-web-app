9d66495ab9818a65f2274318f5d8f84b8bdd5710
Heikki Rantala <heikki.rantala@aalto.fi>
2021-10-08 11:09:05 +0300

    Add images to main page

--- src/client/configs/artsampo/GeneralConfig.js
+++ src/client/configs/artsampo/GeneralConfig.js
@@ -1,4 +1,4 @@
-import bannerImage from '../../img/main_page/mmm-banner.jpg'
+import bannerImage from '../../img/main_page/aino.jpg'
 
 export const rootUrl = ''
 
--- src/client/configs/artsampo/PerspectiveConfig.js
+++ src/client/configs/artsampo/PerspectiveConfig.js
@@ -10,15 +10,13 @@ import PieChartIcon from '@material-ui/icons/PieChart'
 import CloudDownloadIcon from '@material-ui/icons/CloudDownload'
 import BubbleChartIcon from '@material-ui/icons/BubbleChart'
 import LineChartIcon from '@material-ui/icons/ShowChart'
-import manuscriptsImage from '../../img/main_page/manuscripts-452x262.jpg'
-import worksImage from '../../img/main_page/works-452x262.jpg'
-import eventsImage from '../../img/main_page/events-452x262.jpg'
-import placesImage from '../../img/main_page/places-452x262.jpg'
+
+import objectsImage from '../../img/main_page/luxemburg.jpg'
 
 export const perspectiveConfig = [
   {
     id: 'objects',
-    frontPageImage: manuscriptsImage,
+    frontPageImage: objectsImage,
     defaultActiveFacets: new Set([]),
     defaultTab: 'table',
     defaultInstancePageTab: 'table',
